d6b7ceefba7ae30cb939881f670795ed215081f0
root <root@ip-172-31-25-157.us-west-1.compute.internal>
2018-04-25 20:48:27 +0000

    SP03 fixes

--- prep4hostnames.sh
+++ prep4hostnames.sh
@@ -3,11 +3,23 @@
 echo ""
 read -p "Enter fully qualified host name: " fqdn
 
+echo ""
+read -p "Enter organization name: " orgname
+
+echo ""
+read -p "Enter development space name: " devspace
+echo ""
+echo ""
+
 cmd="cd HANA_EXPRESS_20/DATA_UNITS/HDB_LCM_LINUX_X86_64/configurations"
 echo $cmd
 #eval $cmd
 
 cmd="cp auto_install.cfg auto_install_cfg.bak"
+echo $cmd
+#eval $cmd
+
+cmd="cp auto_update.cfg auto_update_cfg.bak"
 echo $cmd
 #eval $cmd
 
@@ -19,17 +31,67 @@ cmd='sed -i -e "s/xs_domain_name=USE_DEFAULT/xs_domain_name='$fqdn'/g" auto_inst
 echo $cmd
 #eval $cmd
 
-echo ""
-read -p "Enter organization name: " orgname
+cmd='sed -i -e "s/org_name=HANAExpress/org_name='$orgname'/g" auto_install.cfg'
+echo $cmd
+#eval $cmd
+
+cmd='sed -i -e "s/org_name=HANAExpress/org_name='$orgname'/g" auto_update.cfg'
+echo $cmd
+#eval $cmd
+
+cmd='sed -i -e "s/prod_space_name=development/prod_space_name='$devspace'/g" auto_install.cfg'
+echo $cmd
+#eval $cmd
+
+cmd='sed -i -e "s/prod_space_name=development/prod_space_name='$devspace'/g" auto_update.cfg'
+echo $cmd
+#eval $cmd
+
+cmd="cd ../../../.."
+echo $cmd
+#eval $cmd
+
+cmd="cd HANA_EXPRESS_20/DATA_UNITS/HDB_SERVER_LINUX_X86_64/configurations"
+echo $cmd
+#eval $cmd
+
+cmd="cp auto_install.cfg auto_install_cfg.bak"
+echo $cmd
+#eval $cmd
+
+cmd="cp auto_update.cfg auto_update_cfg.bak"
+echo $cmd
+#eval $cmd
+
+cmd='sed -i -e "s/xs_routing_mode=ports/xs_routing_mode=hostnames/g" auto_install.cfg'
+echo $cmd
+#eval $cmd
+
+cmd='sed -i -e "s/xs_routing_mode=ports/xs_routing_mode=hostnames/g" auto_update.cfg'
+echo $cmd
+#eval $cmd
+
+cmd='sed -i -e "s/xs_domain_name=USE_DEFAULT/xs_domain_name='$fqdn'/g" auto_install.cfg'
+echo $cmd
+#eval $cmd
+
+cmd='sed -i -e "s/xs_domain_name=USE_DEFAULT/xs_domain_name='$fqdn'/g" auto_update.cfg'
+echo $cmd
+#eval $cmd
 
 cmd='sed -i -e "s/org_name=HANAExpress/org_name='$orgname'/g" auto_install.cfg'
 echo $cmd
 #eval $cmd
 
-echo ""
-read -p "Enter development space name: " devspace
+cmd='sed -i -e "s/org_name=HANAExpress/org_name='$orgname'/g" auto_update.cfg'
+echo $cmd
+#eval $cmd
 
 cmd='sed -i -e "s/prod_space_name=development/prod_space_name='$devspace'/g" auto_install.cfg'
+echo $cmd
+#eval $cmd
+
+cmd='sed -i -e "s/prod_space_name=development/prod_space_name='$devspace'/g" auto_update.cfg'
 echo $cmd
 #eval $cmd
 
